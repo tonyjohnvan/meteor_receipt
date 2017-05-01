550b01c6e6a9d2829395edd1b3b9ac6b96a010f7
tonyjohnvan <tonyjohnvan@gmail.com>
2017-05-01 14:44:20 -0400

    finished

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -12,3 +12,9 @@ Template.Recipes.helpers({
     return Recipes.find({});
   }
 });
+
+Template.Recipes.events({
+  'click .new-recipe': function () {
+    Session.set('newRecipe', true)
+  }
+})
--- collections/Recipes.js
+++ collections/Recipes.js
@@ -61,6 +61,9 @@ Meteor.methods({
         inMenu: !currentState
       }
     })
+  },
+  deleteRecipe: function (id) {
+    Recipes.remove(id)
   }
 })
 
