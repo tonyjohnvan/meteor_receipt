40ff459adfd2666bafd9bbf83183b4063b961739
tonyjohnvan <tonyjohnvan@gmail.com>
2017-04-30 21:48:07 -0400

    #8-12

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -1,28 +1,54 @@
-Recipes = new Meteor.Collection('recipes')
+SimpleSchema.debug = true
+
+Recipes = new Mongo.Collection('recipes')
+
+Recipes.allow({
+  insert: function (userId, doc) {
+    return userId;
+  }
+});
+
+Ingredient = new SimpleSchema({
+  name: {type:String},
+  amount: {type: String}
+})
+
+
 
 RecipeSchema = new SimpleSchema({
   name: {type: String, label: "Name"},
   desc: {type: String, label: "Description"},
+  Ingredients: {
+    type: [Ingredient]
+  },
   author: {
     type: String,
     label: "Author",
-    autoValue: () => {
+    autoValue: function () {
       return this.userId
     },
-    autoform:{
+    autoform: {
+      type: "hidden"
+    }
+  },
+  inMenu: {
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+    autoform: {
       type: "hidden"
     }
   },
   createdAt: {
     type: Date,
     label: "Created At",
-    autoValue: () => {
+    autoValue: function () {
       return new Date()
     },
-    autoform:{
+    autoform: {
       type: "hidden"
     }
   }
 })
 
-Recipes.attachSchema( RecipeSchema );
+Recipes.attachSchema(RecipeSchema);
--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,8 @@
 /**
  * Created by fanzhang on 4/27/17.
  */
+Meteor.publish('recipes', function () {
+  return Recipes.find({
+    author: this.userId
+  })
+})
